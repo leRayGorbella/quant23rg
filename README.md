87554c91e5c3557499c8c9ae41593900403b6c22
AdrienCalas <adrien.calas.pro@gmail.com>
2023-05-30 09:23:50 +0200

    update __init__

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 ## Install it by typing this command in your command prompt 
 
 ```
-pip install quant23rg==0.0.3
+pip install quant23rg==0.0.4
 ```
 
 ## The package is still in progress
--- src/quant23rg/__init__.py
+++ src/quant23rg/__init__.py
@@ -1,6 +1,6 @@
-from implied_volatility import ImpliedVolatility
-from pricingBS import PricingBS
-from pricingCallEuropBS import PricingCallEuropBS
-from pricingPutEuropBS import PricingPutEuropBS
-from pricingGBM import PricingGBM, PricingGBMPortfolio
-from riskManagement import RiskManagementOneAsset, RiskManagementPortfolio
+from quant23rg.implied_volatility import ImpliedVolatility
+from quant23rg.pricingBS import PricingBS
+from quant23rg.pricingCallEuropBS import PricingCallEuropBS
+from quant23rg.pricingPutEuropBS import PricingPutEuropBS
+from quant23rg.pricingGBM import PricingGBM, PricingGBMPortfolio
+from quant23rg.riskManagement import RiskManagementOneAsset, RiskManagementPortfolio
